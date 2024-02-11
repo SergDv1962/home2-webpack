acc03ccf14b4cd823ae29c8a2ac9532bebd28609
unknown <dsv19621227@gmail.com>
2024-02-11 21:37:26 +0200

    add library

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ container.style.padding = "10px";
 const txt = container.firstChild;
 txt.style.padding = '20px';
 txt.style.border = "1px solid red";
+
+let moment = require("moment");
+
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+container.insertAdjacentHTML('beforeend', "<div class='time'></div>");
+const time = document.querySelector('.time');
+time.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
+time.style.color = 'red';
